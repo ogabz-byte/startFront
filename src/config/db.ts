d8cb83a3c66818e2342ca5baf1669aa2c6d97c62
ogabz-byte <ogabidosomto@gmail.com>
2025-05-24 10:36:17 -0700

    ok

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "storfrontdb",
-  port: Number(process.env.DB_PORT),
+  // host: process.env.DB_HOST || "localhost",
+  // user: process.env.DB_USER || "root",
+  // password: process.env.DB_PASSWORD || "",
+  // database: process.env.DB_NAME || "storfrontdb",
+  // port: Number(process.env.DB_PORT),
+
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
